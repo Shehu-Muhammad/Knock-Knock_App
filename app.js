5bcbe3c0e0720364ab1dd5adcbe9689f26bdd71b
Shehu Muhammad <shehumuhammad@my.ccsu.edu>
2022-02-22 18:56:18 -0500

    Rewrote joke.html and joke.js files

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require ('express');
 const app = express();
 const path = require('path');
-const knockknock = require('knock-knock-jokes');
+// const knockknock = require('knock-knock-jokes');
 const port = 3000;
 
 app.use(express.static(`${__dirname}/public`));
 app.get('/', (req,res)=> res.sendFile(path.join(__dirname+'/views/index.html')));
 app.get('/joke', (req,res)=> res.sendFile(path.join(__dirname+'/views/joke.html')));
-console.log(knockknock());
+// console.log(knockknock());
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`));
--- views/joke.js
+++ views/joke.js
@@ -1,43 +1,55 @@
-let p1 = document.getElementById('p1');
-let p2 = document.getElementById('p2');
-let p3 = document.getElementById('p3');
-let p4 = document.getElementById('p4');
-let p5 = document.getElementById('p5');
-let buttonId = document.getElementById("button")
+var p1 = document.getElementById('p1');
+var p2 = document.getElementById('p2');
+var p3 = document.getElementById('p3');
+var p4 = document.getElementById('p4');
+var p5 = document.getElementById('p5');
+let buttonId = document.getElementById("start");
+var number = 0;
 
-var jokes = [
-    {name: 'Dozen', answer: 'anybody want to let me in?'},
-    {name: 'Avenue', answer: 'knocked on this door before?'},
-    {name: 'Ice Cream', answer: 'if you don\'t let me in!'},
-    {name: 'Adore', answer: 'is between us. Open up!'},
-    {name: 'Lettuce', answer: 'in. It\'s cold out here!'},
-    {name: 'Bed', answer: 'you can not guess who I am.'},
-    {name: 'Al', answer: 'give you a kiss if you open the door.'},
-    {name: 'Olive', answer: 'you!'},
-    {name: 'Abby', answer: 'birthday to you!'},
-    {name: 'Rufus', answer: 'the most important part of your house.'},
-    {name: 'Cheese', answer: 'a cute girl.'}
-]
-
-function getJoke(){
-    p1.innerHTML = "Knock, Knock!"
-    p2.innerHTML = "Who's There?"
+function tellJoke()
+{
+        var jokes = [
+        {name: 'Dozen', answer: 'anybody want to let me in?'},
+        {name: 'Avenue', answer: 'knocked on this door before?'},
+        {name: 'Ice Cream', answer: 'if you don\'t let me in!'},
+        {name: 'Adore', answer: 'is between us. Open up!'},
+        {name: 'Lettuce', answer: 'in. It\'s cold out here!'},
+        {name: 'Bed', answer: 'you can not guess who I am.'},
+        {name: 'Al', answer: 'give you a kiss if you open the door.'},
+        {name: 'Olive', answer: 'you!'},
+        {name: 'Abby', answer: 'birthday to you!'},
+        {name: 'Rufus', answer: 'the most important part of your house.'},
+        {name: 'Cheese', answer: 'a cute girl.'}
+    ]
+    
+    if(number === 0)
+    {
+        //do nothing because it is the first joke in the jokes object
+    }
+    else if(number === jokes.length)
+    {
+        number = 0;
+    }
+       
+    let joke = jokes[number];
+    let name = joke.name;
+    p1.innerHTML = "Knock Knock!";
+    p2.innerHTML = "Who's there?";
+    p3.innerHTML = `Person 1: ${name}.`;
+    p4.innerHTML = `Person 2:'${name}' who?`;
+    p5.innerHTML = `Person 1: ${name} ${joke.answer}.`
+    
+    number++;
+    
 }
 
-buttonId.addEventListener("click", getJoke);
-
-
-// function formatJoke(jokes){
-//     setTimeout(,1000)
-// }
-
-// function formatJoke(joke) {
-//     return [
-//       'Knock, knock.',
-//       'Who’s there?',
-//       joke.name + '.',
-//       joke.name + ' who?',
-//       joke.name + ' ' + joke.answer
-//     ].join('\n')
-//   }
-
+function clearJokes() 
+{
+    p1.innerHTML = "";
+    p2.innerHTML = "";
+    p3.innerHTML = "";
+    p4.innerHTML = "";
+    p5.innerHTML = "";
+}
+document.getElementById("start").addEventListener("click", tellJoke);
+document.getElementById("clear").addEventListener("click", clearJokes);
